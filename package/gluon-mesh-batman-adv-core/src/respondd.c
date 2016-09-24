90aa0aeececce0518ea5142f86412233555d2fab
Sven Eckelmann <sven@narfation.org>
2016-09-24 21:00:11 +0200

    gluon-mesh-batman-adv-core: Convert gateway query to netlink

--- package/gluon-mesh-batman-adv-core/src/respondd.c
+++ package/gluon-mesh-batman-adv-core/src/respondd.c
@@ -63,6 +63,11 @@ struct neigh_netlink_opts {
 	struct nlquery_opts query_opts;
 };
 
+struct gw_netlink_opts {
+	struct json_object *obj;
+	struct nlquery_opts query_opts;
+};
+
 
 static struct json_object * get_addresses(void) {
 	FILE *f = fopen("/proc/net/if_inet6", "r");
@@ -208,27 +213,61 @@ static struct json_object * respondd_provider_nodeinfo(void) {
 	return ret;
 }
 
+static const int gateways_mandatory[] = {
+	BATADV_ATTR_ORIG_ADDRESS,
+};
+
+static int parse_gw_list_netlink_cb(struct nl_msg *msg, void *arg)
+{
+	struct nlattr *attrs[BATADV_ATTR_MAX+1];
+	struct nlmsghdr *nlh = nlmsg_hdr(msg);
+	struct nlquery_opts *query_opts = arg;
+	struct genlmsghdr *ghdr;
+	uint8_t *orig;
+	struct gw_netlink_opts *opts;
+	char addr[18];
+
+	opts = container_of(query_opts, struct gw_netlink_opts, query_opts);
+
+	if (!genlmsg_valid_hdr(nlh, 0))
+		return NL_OK;
+
+	ghdr = nlmsg_data(nlh);
+
+	if (ghdr->cmd != BATADV_CMD_GET_GATEWAYS)
+		return NL_OK;
+
+	if (nla_parse(attrs, BATADV_ATTR_MAX, genlmsg_attrdata(ghdr, 0),
+		      genlmsg_len(ghdr), batadv_netlink_policy))
+		return NL_OK;
+
+	if (missing_mandatory_attrs(attrs, gateways_mandatory,
+				    ARRAY_SIZE(gateways_mandatory)))
+		return NL_OK;
+
+	if (!attrs[BATADV_ATTR_FLAG_BEST])
+		return NL_OK;
+
+	orig = nla_data(attrs[BATADV_ATTR_ORIG_ADDRESS]);
+
+	sprintf(addr, "%02x:%02x:%02x:%02x:%02x:%02x",
+		orig[0], orig[1], orig[2], orig[3], orig[4], orig[5]);
+
+	json_object_object_add(opts->obj, "gateway", json_object_new_string(addr));
+
+	return NL_STOP;
+}
 
 static void add_gateway(struct json_object *obj) {
-	FILE *f = fopen("/sys/kernel/debug/batman_adv/bat0/gateways", "r");
-	if (!f)
-		return;
+	struct gw_netlink_opts opts = {
+		.obj = obj,
+		.query_opts = {
+			.err = 0,
+		},
+	};
 
-	char *line = NULL;
-	size_t len = 0;
-
-	while (getline(&line, &len, f) >= 0) {
-		char addr[18];
-
-		if (sscanf(line, "=> %17[0-9a-fA-F:]", addr) != 1)
-			continue;
-
-		json_object_object_add(obj, "gateway", json_object_new_string(addr));
-		break;
-	}
-
-	free(line);
-	fclose(f);
+	netlink_query_common("bat0", BATADV_CMD_GET_GATEWAYS,
+			     parse_gw_list_netlink_cb, &opts.query_opts);
 }
 
 static inline bool ethtool_ioctl(int fd, struct ifreq *ifr, void *data) {
